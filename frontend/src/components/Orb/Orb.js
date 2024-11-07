b417111e66848d58dfeabe79285bac7393401184
kubraacelik <kkbra.celik92@gmail.com>
2024-11-07 13:57:09 +0300

    created

---
+++ frontend/src/components/Orb/Orb.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from "styled-components"
+
+function Orb() {
+
+  const OrbStyled = styled.div`
+  `;
+
+  return (
+    <OrbStyled></OrbStyled>
+  )
+}
+
+export default Orb
