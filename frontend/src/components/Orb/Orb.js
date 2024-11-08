69edd2dbc3be26ea0c5687009092add503724a3a
kubraacelik <kkbra.celik92@gmail.com>
2024-11-08 11:25:35 +0300

    created animation

--- frontend/src/components/Orb/Orb.js
+++ frontend/src/components/Orb/Orb.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import { useWindowSize } from "../../utils/useWindowSize";
 
 function Orb() {
+  const { width, height } = useWindowSize();
 
-  const OrbStyled = styled.div`
+  // Burada moveOrb adında bir animasyon tanımlanmıştır
+  const moveOrb = keyframes`
+    0%{
+      transform: translate(0, 0);
+    }
+    50%{
+      transform: translate(${width/1.2}px, ${height/2}px);
+    }
+    100%{
+      transform: translate(0, 0);
+    }
   `;
 
-  return (
-    <OrbStyled></OrbStyled>
-  )
+  const OrbStyled = styled.div`
+    width: 70vh;
+    height: 70vh;
+    position: absolute;
+    border-radius: 50%;
+    margin-left: -37vh;
+    margin-top: -37vh;
+    background: linear-gradient(180deg, #f56692 0%, #f2994a 100%);
+    filter: blur(200px);
+    animation: ${moveOrb} 7s alternate linear infinite;
+  `;
+
+  return <OrbStyled>hello</OrbStyled>;
 }
 
-export default Orb
+export default Orb;
