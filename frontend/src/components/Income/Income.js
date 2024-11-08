5428201b55af1f8a3fc3c5b675e879a317b8a492
kubraacelik <kkbra.celik92@gmail.com>
2024-11-08 17:44:33 +0300

    provides the user with a form listing revenues and adding new revenues

---
+++ frontend/src/components/Income/Income.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import { InnerLayout } from "../../styles/Layouts";
+import { useGlobalContext } from "../../context/globalContext";
+import Form from "../Form/Form";
+
+function Income() {
+  const { addIncome, incomes, getIncomes } = useGlobalContext();
+
+  useEffect(() => {
+    getIncomes();
+  }, []);
+
+  return (
+    <IncomeStyled>
+      <InnerLayout>
+        <h1>Income</h1>
+        <div className="income-content">
+          <div className="form-container">
+            <Form />
+          </div>
+          <div className="incomes">
+            {incomes.map((income) => {
+              const { id, title, amount, date, category, description } = income;
+              return <Income />;
+            })}
+          </div>
+        </div>
+      </InnerLayout>
+    </IncomeStyled>
+  );
+}
+
+const IncomeStyled = styled.div``;
+
+export default Income;
