6140f81add74fdef73502675e77507c51cc9bcb8
kubraacelik <kkbra.celik92@gmail.com>
2024-11-08 17:40:03 +0300

    created components

---
+++ frontend/src/components/IncomeItem/IncomeItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function IncomeItem() {
+  return (
+    <IncomeItemStyled>IncomeItem</IncomeItemStyled>
+  )
+}
+
+const IncomeItemStyled = styled.div``;
+
+export default IncomeItem
