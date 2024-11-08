ea7d4899644b676f584a3b82e740a702fb2875e3
kubraacelik <kkbra.celik92@gmail.com>
2024-11-08 17:26:06 +0300

    added component to be shown according to active status

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,46 @@ import bg from "./img/bg.png";
 import { MainLayout } from "./styles/Layouts";
 import Orb from "./components/Orb/Orb";
 import Navigation from "./components/Navigation/Navigation";
+import { useMemo, useState } from "react";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Expenses from "./components/Expenses/Expenses";
+import Income from "./components/Income/Income";
+import { useGlobalContext } from "./context/globalContext";
 
 function App() {
+  // aktif olan sekmeyi belirtir
+  const [active, setActive] = useState(1);
+
+  const global = useGlobalContext();
+  console.log(global);
+
+  // active durumuna göre gösterilecek bileşen
+  const displayData = () => {
+    switch (active) {
+      case 1:
+        return <Dashboard />;
+      case 2:
+        return <Dashboard />;
+      case 3:
+        return <Income />;
+      case 4:
+        return <Expenses />;
+      default:
+        return <Dashboard />;
+    }
+  };
+
+  // Orb bileşeni sadece ilk render sırasında bir kez oluşturulur ve sonra hafızada saklanır
+  const orbMemo = useMemo(() => {
+    return <Orb />;
+  }, []);
+
   return (
     <AppStyled bg={bg} className="App">
-      <Orb />
+      {orbMemo}
       <MainLayout>
-        <Navigation />
+        <Navigation active={active} setActive={setActive} />
+        <main>{displayData()}</main>
       </MainLayout>
     </AppStyled>
   );
@@ -20,6 +53,18 @@ const AppStyled = styled.div`
   background-image: url(${(props) => props.bg});
   background-size: cover;
   position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow: auto;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
 `;
 
 export default App;
