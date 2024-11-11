c0ebec4170486410ccc3b817716fd43f1957e634
kubraacelik <kkbra.celik92@gmail.com>
2024-11-11 19:36:27 +0300

    We have given the width and height of the browser window as the initial value of state

--- frontend/src/utils/useWindowSize.js
+++ frontend/src/utils/useWindowSize.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const useWindowSize = () => {
   // pencere boyutlarını tutar
-  const [size, setSize] = useState([0, 0]);
+  const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
 
   useEffect(() => {
     // pencere boyutları değiştiğinde çağrılır
