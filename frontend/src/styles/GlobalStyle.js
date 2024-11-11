25c3bcda27a86bd395fe58f6a9323ad98cd16cd8
kubraacelik <kkbra.celik92@gmail.com>
2024-11-11 19:52:03 +0300

    given css codes for error

--- frontend/src/styles/GlobalStyle.js
+++ frontend/src/styles/GlobalStyle.js
@@ -1,4 +1,3 @@
-// projenin tamamında geçerli olacak global CSS stillerini tanımlar
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
@@ -11,8 +10,8 @@ export const GlobalStyle = createGlobalStyle`
 
     :root{
         --primary-color: #222260;
-        --primary-color2: 'color: rgba(34, 34, 96, .6)';
-        --primary-color3: 'color: rgba(34, 34, 96, .4)';
+        --primary-color2: rgba(34, 34, 96, .6);
+        --primary-color3: rgba(34, 34, 96, .4);
         --color-green: #42AD00;
         --color-grey: #aaa;
         --color-accent: #F56692;
@@ -23,10 +22,32 @@ export const GlobalStyle = createGlobalStyle`
         font-family: "Nunito", sans-serif;
         font-size: clamp(1rem, 1.5vw, 1.2rem);
         overflow: hidden;
-        color: rgba(34, 34, 96, .6);
+        color: var(--primary-color2);
     }
 
     h1, h2, h3, h4, h5, h6 {
         color: var(--primary-color);
+    }
+
+    .error{
+        color: red;
+        animation: shake 0.5s ease-in-out;
+        @keyframes shake {
+            0% {
+                transform: translateX(0);
+            }
+            25% {
+                transform: translateX(10px);
+            }
+            50% {
+                transform: translateX(-10px);
+            }
+            75% {
+                transform: translateX(10px);
+            }
+            100% {
+                transform: translateX(0);
+            }
+        }
     }
 `;
